5c59c458401d1bf00de66e9866423150e29a7b80
martijnxd <49381565+martijnxd@users.noreply.github.com>
2021-01-01 18:40:28 +0100

    credz

--- set_credz.ps1
+++ set_credz.ps1
@@ -1,6 +1,14 @@
-param($credential)
-if (!$credential) { $credential = get-creential }
-$env:GOVMOMI_URL = "https://vc.mvdp.eu/sdk"
-$env:GOVMOMI_USERNAME = $($Credential.username)
-$env:GOVMOMI_PASSWORD = $($Credential.GetNetworkCredential().Password)
-$env:GOVMOMI_INSECURE = "true"
+param($credential, $prefix = "GOVC", $vchost)
+if (!$credential) { $credential = get-credential }
+if ($prefix -eq "GOVC") {
+    $env:GOVC_URL = "https://" + $vchost
+    $env:GOVC_USERNAME = $($Credential.username)
+    $env:GOVC_PASSWORD = $($Credential.GetNetworkCredential().Password)
+    $env:GOVC_INSECURE = "true"
+}
+if ($prefix -eq "GOVMOMI") {
+    $env:GOVMOMI_URL = "https://" + $vchost + "/sdk"
+    $env:GOVMOMI_USERNAME = $($Credential.username)
+    $env:GOVMOMI_PASSWORD = $($Credential.GetNetworkCredential().Password)
+    $env:GOVMOMI_INSECURE = "true"
+}
