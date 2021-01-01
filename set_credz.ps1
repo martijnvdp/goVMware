ba5617811dc0ef082dc03f3f1dec831de6cd0ea1
martijnxd <49381565+martijnxd@users.noreply.github.com>
2021-01-01 18:12:16 +0100

    chnaged cred

--- set_credz.ps1
+++ set_credz.ps1
@@ -1,5 +1,6 @@
-param($user,$pass)
-$env:GOVMOMI_URL="https://vc.mvdp.eu/sdk"
-$env:GOVMOMI_USERNAME=$user
-$env:GOVMOMI_PASSWORD=$pass
-$env:GOVMOMI_INSECURE="true"
+param($credential)
+if !($user) { $credential = get-creential }
+$env:GOVMOMI_URL = "https://vc.mvdp.eu/sdk"
+$env:GOVMOMI_USERNAME = $($Credential.username)
+$env:GOVMOMI_PASSWORD = $($Credential.GetNetworkCredential().Password)
+$env:GOVMOMI_INSECURE = "true"
